d60227575e03a3fa066ae88a9f73c624bc598f43
mjsprengel <mjsprengel713@gmail.com>
2017-10-16 01:24:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -25,22 +25,20 @@ function draw(){
     // for each body, detect if it is colliding with another body, and if so, add mass and transfer momentum 
     for(let i = 0; i < planets.length; i++){
         for(let j = 0; j < planets.length; j++){
-            if(j!=i){
-                if(p5.Vector.dist(planets[i].position, planets[j].position) < (planets[i].radius + planets[j].radius)){
-                    if(planets[i].radius > planets[j].radius){
+            if(p5.Vector.dist(planets[i].position, planets[j].position) < (planets[i].radius + planets[j].radius) && j!=i){
+                if(planets[i].radius > planets[j].radius){
 
-                        planets[i].set_vel(calc_momentum_transfer(planets[i].mass, planets[j].mass, planets[i].velocity, planets[j].velocity));  
-                        planets[i].add_mass(planets[j].mass);  
-                        planets.splice(j,1); // delete the smaller body from the array of bodies
-                        break; //break out of inner for loop, this body is colliding with another body and was deleted
+                    planets[i].set_vel(calc_momentum_transfer(planets[i].mass, planets[j].mass, planets[i].velocity, planets[j].velocity));  
+                    planets[i].add_mass(planets[j].mass);  
+                    planets.splice(j,1); // delete the smaller body from the array of bodies
+                    break; //break out of inner for loop, this body is colliding with another body and was deleted
 
-                    } else {
+                } else {
 
-                        planets[j].set_vel(calc_momentum_transfer(planets[i].mass, planets[j].mass, planets[i].velocity, planets[j].velocity));
-                        planets[j].add_mass(planets[i].mass);
-                        planets.splice(i,1);
-                        break; //break out of inner for loop - otherwise program will crash at seemingly random times with multiple collisions
-                    }
+                    planets[j].set_vel(calc_momentum_transfer(planets[i].mass, planets[j].mass, planets[i].velocity, planets[j].velocity));
+                    planets[j].add_mass(planets[i].mass);
+                    planets.splice(i,1);
+                    break; //break out of inner for loop - otherwise program will crash at seemingly random times with multiple collisions
                 }
             }
         }
